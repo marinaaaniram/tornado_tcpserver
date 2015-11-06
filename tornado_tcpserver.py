0e0e3d195e5dbf60c58b2d24d811e269d54f6413
Your Name <you@example.com>
2015-11-06 11:30:08 +0300

    Web interface.

--- tornado_tcpserver.py
+++ tornado_tcpserver.py
@@ -7,6 +7,12 @@ from tornado.httpserver import HTTPServer
 from tornado.websocket import WebSocketHandler
 from tornado.web import Application, RequestHandler
 from tornado.ioloop import IOLoop
+from tornado.options import define, options
+
+
+define('tcp_port', default=5555)
+define('http_port', default=8888)
+define('host', default='localhost')
 
 
 class SimpleTcpClient(object):
@@ -37,23 +43,27 @@ class SimpleTcpClient(object):
             src_name = ''
             while True:
                 message = yield self.stream.read_until(b'\n')
-                msg = message.split('::')
 
-                if msg[0] == 'Auth':
-                    is_auth = True
-                    src_name = msg[1].split('\n')[0]
-                elif msg[0] == 'End':
-                    is_auth = False
-                elif is_auth:
-                    msg_key = msg[0].decode('utf-8').strip()
-                    msg_value = msg[1].decode('utf-8').strip()
+                if message == 'End\n':
+                	is_auth = False
+                	continue
 
-                    self.log(src_name, '%s | %s' % (msg_key, msg_value))
+                msg = message.split(':: ')
 
-                    # [con.write_message('Yeah!!!') for con in WSHandler.connections]
+                if msg[0] == 'Auth' and len(msg) > 1:
+                	is_auth = True
+                	src_name = msg[1].split('\n')[0]
+                	continue
+
+                if is_auth and len(msg) > 1:
+	                msg_key = msg[0].decode('utf-8').strip()
+	                msg_value = msg[1].decode('utf-8').strip()
+
+	                self.log(src_name, '%s | %s' % (msg_key, msg_value))
+	                [con.write_message('[%s] %s | %s' % (src_name, msg_key, msg_value)) for con in WSHandler.connections]
 
         except StreamClosedError:
-            self.log('Error: ', 'StreamClosedError')
+            self.log('Error', 'StreamClosedError')
 
 
 class SimpleTcpServer(TCPServer):
@@ -90,18 +100,14 @@ application = Application([
 
 
 def main():
-    # configuration
-    host = 'localhost'
-    port = 8008
-
-    # # http server
+    # http server
     http_server = HTTPServer(application)
-    http_server.listen(8888)
+    http_server.listen(options.http_port, options.host)
 
     # tcp server
     server = SimpleTcpServer()
-    server.listen(port, host)
-    print("Listening on %s:%d..." % (host, port))
+    server.listen(options.tcp_port, options.host)
+    print('Server started...')
 
     # infinite loop
     IOLoop.instance().start()
